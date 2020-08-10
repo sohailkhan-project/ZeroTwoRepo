9d3a921005f6812937beda5c9621c2c099648ea6
Harsh17786 <68142717+Harsh17786@users.noreply.github.com>
2020-08-10 17:28:20 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -32,14 +32,14 @@ The support group chat is at {}.
 
 HELP_STRINGS = """
 Hey there! My name is *{}*.
-I'm a Demon girl who help admins manage their groups with my powers! Have a look at the following for an idea of some of \
-the things I can help you with.
+I am a group management bot, here to help you get around and keep the order in your groups! Have a look at the following for an idea of some of \
+Have a look at the following for an idea of some of 
+\the things I can help you with.
 
 *Main* commands available:
  • /start: start the bot
  • /help: PM's you this message.
  • /help <module name>: PM's you info about that module.
-
  • /settings:
    • in PM: will send you your settings for all supported modules.
    • in a group: will redirect you to pm, with all that chat's settings.
