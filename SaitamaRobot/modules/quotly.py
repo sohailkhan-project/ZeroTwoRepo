897d628a456a01e69abe12c152a545e35f86e3c9
Harsh17786 <68142717+Harsh17786@users.noreply.github.com>
2020-08-26 15:09:40 +0530

    Update quotly.py

--- SaitamaRobot/modules/quotly.py
+++ SaitamaRobot/modules/quotly.py
@@ -10,7 +10,7 @@ import random
 import json
 import os
 import re
-from saitamaRobot.events import register
+from saitamaRobot.saitamaRobot import saitamaRobot
 
 COLORS = [
     "#F07975", "#F49F69", "#F9C84A", "#8CC56E", "#6CC7DC", "#80C1FA", "#BCB3F9", "#E181AC"]
