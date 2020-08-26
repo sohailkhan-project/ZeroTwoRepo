8fbbd1d496c3dac3925c80d0fc8303d252a501d0
Harsh17786 <68142717+Harsh17786@users.noreply.github.com>
2020-08-26 15:16:06 +0530

    Update quotly.py

--- SaitamaRobot/modules/quotly.py
+++ SaitamaRobot/modules/quotly.py
@@ -1,7 +1,7 @@
 from PIL import Image, ImageDraw, ImageFont, ImageOps
 from telethon.tl import types, functions
 from fontTools.ttLib import TTFont 
-from  fontTools.unicode import Unicode 
+from fontTools.unicode import Unicode 
 import emoji
 import textwrap
 import urllib
