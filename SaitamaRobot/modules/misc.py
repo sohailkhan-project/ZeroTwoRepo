8eded1d954f90c70497a1b7254ab5af3e10a1189
Harsh17786 <68142717+Harsh17786@users.noreply.github.com>
2020-09-14 17:11:17 +0530

    Update misc.py

--- SaitamaRobot/modules/misc.py
+++ SaitamaRobot/modules/misc.py
@@ -262,7 +262,7 @@ dispatcher.add_handler(MD_HELP_HANDLER)
 dispatcher.add_handler(STATS_HANDLER)
 
 __mod_name__ = "Misc"
-__command_list__ = ["id", "info", "echo"]
+__command_list__ = ["id", "info", "pat" "echo"]
 __handlers__ = [
     ID_HANDLER, GIFID_HANDLER, INFO_HANDLER, ECHO_HANDLER, PAT_HANDLER, MD_HELP_HANDLER,
     STATS_HANDLER
