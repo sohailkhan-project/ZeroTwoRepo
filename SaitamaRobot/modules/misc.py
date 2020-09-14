7a6e0d71f5a9ac111254628e3e3dbbeb5c372c6b
Harsh17786 <68142717+Harsh17786@users.noreply.github.com>
2020-09-14 17:29:24 +0530

    Update misc.py

--- SaitamaRobot/modules/misc.py
+++ SaitamaRobot/modules/misc.py
@@ -248,6 +248,7 @@ __help__ = """
 ID_HANDLER = DisableAbleCommandHandler("id", get_id)
 GIFID_HANDLER = DisableAbleCommandHandler("gifid", gifid)
 INFO_HANDLER = DisableAbleCommandHandler(["info"], info)
+PAT_HANDLER = DisableAbleCommandHandler("pat", pat)
 ECHO_HANDLER = DisableAbleCommandHandler("echo", echo, filters=Filters.group)
 MD_HELP_HANDLER = CommandHandler(
     "markdownhelp", markdown_help, filters=Filters.private)
