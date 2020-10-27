15c66404609ae6df0e6bfc685a5bae16c65738df
Harsh17786 <68142717+Harsh17786@users.noreply.github.com>
2020-10-27 15:56:07 +0530

    Update purge.py

--- SaitamaRobot/modules/purge.py
+++ SaitamaRobot/modules/purge.py
@@ -1,6 +1,6 @@
 from SaitamaRobot.modules.helper_funcs.telethn.chatstatus import (
     can_delete_messages, user_is_admin)
-from SaitamaRobot.SaitamaRobot import Saitama
+from SaitamaRobot.Saitamabot import Saitama
 
 
 @megumi(pattern="^/purge$")
