6f9fd62f8436a6df30c85a0ca70b4b4fe7eae116
Harsh17786 <68142717+Harsh17786@users.noreply.github.com>
2020-10-31 16:13:46 +0530

    Update purge.py

--- SaitamaRobot/modules/purge.py
+++ SaitamaRobot/modules/purge.py
@@ -6,74 +6,60 @@ from telethon import events
 
 
 @telethn.on(events.NewMessage(pattern="^[!/]purge$"))
-async def purge(event):
-    chat = event.chat_id
+async def purge_messages(event):
     start = time.perf_counter()
-    msgs = []
-
-    if not await is_administrator(user_id=event.from_id, message=event):
-        await event.reply("You're not an admin!")
+    if event.from_id is None:
         return
 
-    msg = await event.get_reply_message()
-    if not msg:
-        await event.reply("Reply to a message to select where to start purging from.")
+    if not await can_delete_messages(message=event):
+        await event.reply("Can't seem to purge the message")
         return
 
-    try:
-        msg_id = msg.id
-        count = 0
-        to_delete = event.message.id - 1
-        await event.client.delete_messages(chat, event.message.id)
-        msgs.append(event.reply_to_msg_id)
-        for m_id in range(to_delete, msg_id - 1, -1):
-            msgs.append(m_id)
-            count += 1
-            if len(msgs) == 100:
-                await event.client.delete_messages(chat, msgs)
-                msgs = []
+    reply_msg = await event.get_reply_message()
+    if not reply_msg:
+        await event.reply(
+            "Reply to a message to select where to start purging from.")
+        return
+    messages = []
+    message_id = reply_msg.id
+    delete_to = event.message.id
 
-        await event.client.delete_messages(chat, msgs)
-        time_ = time.perf_counter() - start
-        del_res = await event.client.send_message(
-            event.chat_id, f"Purged {count} messages in {time_:0.2f} Secs."
-        )
+    messages.append(event.reply_to_msg_id)
+    for msg_id in range(message_id, delete_to + 1):
+        messages.append(msg_id)
+        if len(messages) == 100:
+            await event.client.delete_messages(event.chat_id, messages)
+            messages = []
 
-        await asyncio.sleep(4)
-        await del_res.delete()
-
-    except MessageDeleteForbiddenError:
-        text = "Failed to delete messages.\n"
-        text += "Messages maybe too old or I'm not admin! or dont have delete rights!"
-        del_res = await event.respond(text, parse_mode="md")
-        await asyncio.sleep(5)
-        await del_res.delete()
+    await event.client.delete_messages(event.chat_id, messages)
+    time_ = time.perf_counter() - start
+    text = f"Purged Successfully in {time_:0.2f} Second(s)"
+    await event.respond(text, parse_mode='markdown')
 
 
 @telethn.on(events.NewMessage(pattern="^[!/]del$"))
-async def delete_msg(event):
-
-    if not await is_administrator(user_id=event.from_id, message=event):
-        await event.reply("You're not an admin!")
+async def delete_messages(event):
+    if event.from_id is None:
         return
 
-    chat = event.chat_id
-    msg = await event.get_reply_message()
-    if not msg:
-        await event.reply("Reply to some message to delete it.")
+    if not await can_delete_messages(message=event):
+        await event.reply("Can't seem to delete this?")
         return
-    to_delete = event.message
+
+    message = await event.get_reply_message()
+    if not message:
+        await event.reply("Whadya want to delete?")
+        return
     chat = await event.get_input_chat()
-    rm = [msg, to_delete]
-    await event.client.delete_messages(chat, rm)
+    del_message = [message, event.message]
+    await event.client.delete_messages(chat, del_message)
 
 
 __help__ = """
-Deleting messages made easy with this command. Bot purges \
-messages all together or individually.
 *Admin only:*
- - /del: Deletes the message you replied to
- - /purge: Deletes all messages between this and the replied to message.
+ - /del: deletes the message you replied to
+ - /purge: deletes all messages between this and the replied to message.
+ - /purge <integer X>: deletes the replied message, and X messages following it if replied to a message.
 """
 
 __mod_name__ = "Purges"
