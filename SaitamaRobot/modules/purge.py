39d379344e5de2d9e17589db61d07c1563a2bc28
Harsh17786 <68142717+Harsh17786@users.noreply.github.com>
2020-10-31 16:08:16 +0530

    Update purge.py

--- SaitamaRobot/modules/purge.py
+++ SaitamaRobot/modules/purge.py
@@ -1,31 +1,11 @@
-import html
-
-from SaitamaRobot import SUDO_USERS, dispatcher
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
-from SaitamaRobot.modules.helper_funcs.chat_status import (bot_admin, can_pin,
-                                                           can_promote,
-                                                           connection_status,
-                                                           user_admin)
-from SaitamaRobot.modules.helper_funcs.extraction import (extract_user,
-                                                          extract_user_and_text)
-from SaitamaRobot.modules.log_channel import loggable
-from telegram import ParseMode, Update
-from telegram.error import BadRequest
-from telegram.ext import CallbackContext, CommandHandler, Filters, run_async
-from telegram.utils.helpers import mention_html
-# Check if user has admin rights
-async def is_administrator(user_id: int, message):
-    admin = False
-    async for user in client.iter_participants(
-        message.chat_id, filter=ChannelParticipantsAdmins
-    ):
-        if user_id == user.id or user_id in SUDO_USERS:
-            admin = True
-            break
-    return admin
+from SaitamaRobot.modules.helper_funcs.telethn.chatstatus import (
+    can_delete_messages, user_is_admin)
+from SaitamaRobot import telethn
+import time
+from telethon import events
 
 
-@client.on(events.NewMessage(pattern="^[!/]purge$"))
+@telethn.on(events.NewMessage(pattern="^[!/]purge$"))
 async def purge(event):
     chat = event.chat_id
     start = time.perf_counter()
@@ -91,7 +71,6 @@ async def delete_msg(event):
 __help__ = """
 Deleting messages made easy with this command. Bot purges \
 messages all together or individually.
-
 *Admin only:*
  - /del: Deletes the message you replied to
  - /purge: Deletes all messages between this and the replied to message.
