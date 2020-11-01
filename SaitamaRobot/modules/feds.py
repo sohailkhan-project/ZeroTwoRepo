319b467bfda9fbb51653647329e6acd471c830fe
Harsh17786 <68142717+Harsh17786@users.noreply.github.com>
2020-11-01 15:27:44 +0530

    Update feds.py

--- SaitamaRobot/modules/feds.py
+++ SaitamaRobot/modules/feds.py
@@ -8,8 +8,8 @@ from io import BytesIO
 import html
 
 import SaitamaRobot.modules.sql.feds_sql as sql
-from SaitamaRobot import (EVENT_LOGS, LOGGER, OWNER_ID, DRAGONS, TIGERS, WOLVES,
-                          dispatcher)
+from SaitamaRobot import (EVENT_LOGS, LOGGER, OWNER_ID, SUDO_USERS, TIGER_USERS,
+                          WHITELIST_USERS, dispatcher)
 from SaitamaRobot.modules.disable import DisableAbleCommandHandler
 from SaitamaRobot.modules.helper_funcs.alternate import send_message
 from SaitamaRobot.modules.helper_funcs.chat_status import is_user_admin
