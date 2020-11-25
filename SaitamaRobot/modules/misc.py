2c7fade901e4720b1ae5aff6e9173689b9cfe8f3
OwO-svg334 <74977067+OwO-svg334@users.noreply.github.com>
2020-11-25 18:13:04 +0530

    Update misc.py

--- SaitamaRobot/modules/misc.py
+++ SaitamaRobot/modules/misc.py
@@ -97,89 +97,6 @@ def gifid(update: Update, context: CallbackContext):
         update.effective_message.reply_text(
             "Please reply to a gif to get its ID.")
 
-
-@run_async
-def info(update: Update, context: CallbackContext):
-    bot, args = context.bot, context.args
-    message = update.effective_message
-    chat = update.effective_chat
-    user_id = extract_user(update.effective_message, args)
-
-    if user_id:
-        user = bot.get_chat(user_id)
-
-    elif not message.reply_to_message and not args:
-        user = message.from_user
-
-    elif not message.reply_to_message and (
-            not args or
-        (len(args) >= 1 and not args[0].startswith("@") and
-         not args[0].isdigit() and
-         not message.parse_entities([MessageEntity.TEXT_MENTION]))):
-        message.reply_text("I can't extract a user from this.")
-        return
-
-    else:
-        return
-
-    text = (f"<b>Characteristics:</b>\n"
-            f"ID: <code>{user.id}</code>\n"
-            f"First Name: {html.escape(user.first_name)}")
-
-    if user.last_name:
-        text += f"\nLast Name: {html.escape(user.last_name)}"
-
-    if user.username:
-        text += f"\nUsername: @{html.escape(user.username)}"
-
-    text += f"\nPermanent user link: {mention_html(user.id, 'link')}"
-
-    if user.id == OWNER_ID:
-        text += "\nThis person is my owner - I would never do anything against them!."
-        
-    elif user.id in DEV_USERS:
-        text += "\nThis person is my dev - I would never do anything against them!."
-        
-    elif user.id in DRAGONS:
-        text += "\nThis person is one of my sudo users! " \
-                    "Nearly as powerful as my owner - so watch it.."
-        
-    elif user.id in DEMONS:
-        text += "\nThis person is one of my support users! " \
-                        "Not quite a sudo user, but can still gban you off the map."
-
-    try:
-        user_member = chat.get_member(user.id)
-        if user_member.status == 'administrator':
-            result = requests.post(
-                f"https://api.telegram.org/bot{TOKEN}/getChatMember?chat_id={chat.id}&user_id={user.id}"
-            )
-            result = result.json()["result"]
-            if "custom_title" in result.keys():
-                custom_title = result['custom_title']
-                text += f"\n\nThis user holds the title <b>{custom_title}</b> here."
-    except BadRequest:
-        pass
-
-    for mod in USER_INFO:
-        try:
-            mod_info = mod.__user_info__(user.id).strip()
-        except TypeError:
-            mod_info = mod.__user_info__(user.id, chat.id).strip()
-        if mod_info:
-            text += "\n\n" + mod_info
-
-    if user.id in [777000, 1087968824]:
-        text += "╘══「 Groups count: <code>???</code> 」"
-    if user.id == dispatcher.bot.id:
-        text += "╘══「 Groups count: <code>???</code> 」"
-    num_chats = sql.get_user_num_chats(user.id)
-    text += f"""╘══「 Groups count: <code>{num_chats}</code> 」"""
-
-
-    update.effective_message.reply_text(
-        text, parse_mode=ParseMode.HTML, disable_web_page_preview=True)
-
 @run_async
 def pat(update: Update, context: CallbackContext):
     bot, args = context.bot, context.args
@@ -238,7 +155,6 @@ def stats(update: Update, context: CallbackContext):
 __help__ = """
  • `/id`*:* get the current group id. If used by replying to a message, gets that user's id.
  • `/gifid`*:* reply to a gif to me to tell you its file ID.
- • `/info`*:* get information about a user.
  • `/markdownhelp`*:* quick summary of how markdown works in telegram - can only be called in private chats.
 """
 
@@ -253,15 +169,14 @@ STATS_HANDLER = CommandHandler("stats", stats)
 
 dispatcher.add_handler(ID_HANDLER)
 dispatcher.add_handler(GIFID_HANDLER)
-dispatcher.add_handler(INFO_HANDLER)
 dispatcher.add_handler(PAT_HANDLER)
 dispatcher.add_handler(ECHO_HANDLER)
 dispatcher.add_handler(MD_HELP_HANDLER)
 dispatcher.add_handler(STATS_HANDLER)
 
 __mod_name__ = "Misc"
-__command_list__ = ["id", "info", "pat" "echo"]
+__command_list__ = ["id", "pat", "echo"]
 __handlers__ = [
-    ID_HANDLER, GIFID_HANDLER, INFO_HANDLER, PAT_HANDLER,
+    ID_HANDLER, GIFID_HANDLER, PAT_HANDLER,
     ECHO_HANDLER, MD_HELP_HANDLER, STATS_HANDLER
 ]
