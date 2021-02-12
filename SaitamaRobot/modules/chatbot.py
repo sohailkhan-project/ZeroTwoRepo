b7fd770f48a892fff3dc4f355e04649d975eeca6
Harsh17786 <68142717+Harsh17786@users.noreply.github.com>
2021-02-12 19:45:58 +0530

    Update chatbot.py

--- SaitamaRobot/modules/chatbot.py
+++ SaitamaRobot/modules/chatbot.py
@@ -80,7 +80,7 @@ def remove_chat(update: Update, context: CallbackContext):
 
 def check_message(context: CallbackContext, message):
     reply_msg = message.reply_to_message
-    if message.text.lower() == "Rem":
+    if message.text.lower() == "saitama":
         return True
     if reply_msg:
         if reply_msg.from_user.id == context.bot.get_me().id:
@@ -142,7 +142,7 @@ def list_chatbot_chats(update: Update, context: CallbackContext):
 
 
 __help__ = f"""
-Chatbot utilizes the CoffeeHouse API and allows Rem to talk and provides a more interactive group chat experience.
+Chatbot utilizes the CoffeeHouse API and allows Saitama to talk and provides a more interactive group chat experience.
 *Commands:* 
 *Admins only:*
  • `/addchat`*:* Enables Chatbot mode in the chat.
