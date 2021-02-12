37586ebb4ab1d9e11044813392d5b8c0c41272ea
Dark Samurai <73326139+ninja7-bot@users.noreply.github.com>
2021-02-12 15:59:53 +0530

    Update reverse.py

--- SaitamaRobot/modules/reverse.py
+++ SaitamaRobot/modules/reverse.py
@@ -182,7 +182,7 @@ def scam(imgspage, lim):
 
 
 REVERSE_HANDLER = DisableAbleCommandHandler(
-    "reverse", reverse, pass_args=True, admin_ok=True
+    "reverse", "grs" , reverse, pass_args=True, admin_ok=True
 )
 
 dispatcher.add_handler(REVERSE_HANDLER)
