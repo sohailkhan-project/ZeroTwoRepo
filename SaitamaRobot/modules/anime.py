50967335eb25432a28caf3a04ac41eae8a453bd6
Dark Samurai <73326139+ninja7-bot@users.noreply.github.com>
2021-04-14 19:35:30 +0530

    Update anime.py

--- SaitamaRobot/modules/anime.py
+++ SaitamaRobot/modules/anime.py
@@ -567,17 +567,17 @@ def site_search(update: Update, context: CallbackContext, site: str):
             post_name = html.escape(entry.text.strip())
             result += f"• <a href='{post_link}'>{post_name}</a>\n"
             
-    elif site == "katana":
-        search_url = f"https://mangakatana.com/?search={search_query}&search_by=book_name"
+    elif site == "mutiny":
+        search_url = f"https://mangamutiny.org/title/{search_query}"
         html_text = requests.get(search_url).text
         soup = bs4.BeautifulSoup(html_text, "html.parser")
         search_result = soup.find_all("h2", {'class': "title"})
 
-        result = f"<b>Results for</b> <code>{html.escape(search_query)}</code> <b>on</b> <code>MangaKatana</code>: \n"
+        result = f"<b>Results for</b> <code>{html.escape(search_query)}</code> <b>on</b> <code>MangaMutiny</code>: \n"
         for entry in search_result:
 
             if entry.text.strip() == "Nothing Found":
-                result = f"<b>No result found for</b> <code>{html.escape(search_query)}</code> <b>on</b> <code>MangaKatana</code>"
+                result = f"<b>No result found for</b> <code>{html.escape(search_query)}</code> <b>on</b> <code>MangaMutiny</code>"
                 more_results = False
                 break
 
@@ -619,8 +619,8 @@ def erai(update: Update, context: CallbackContext):
     site_search(update, context, "erai")
    
 @run_async
-def katana(update: Update, context: CallbackContext):
-    site_search(update, context, "katana")
+def mutiny(update: Update, context: CallbackContext):
+    site_search(update, context, "mutiny")
 
 __help__ = """
 Get information about anime, manga or characters from [AniList](anilist.co).
@@ -648,7 +648,7 @@ KAIZOKU_SEARCH_HANDLER = DisableAbleCommandHandler("kaizoku", kaizoku)
 KAYO_SEARCH_HANDLER = DisableAbleCommandHandler("kayo", kayo)
 VIBE_SEARCH_HANDLER = DisableAbleCommandHandler("vibe", vibe)
 ERAI_SEARCH_HANDLER = DisableAbleCommandHandler("erai", erai)
-KATANA_HANDLER = DisableAbleCommandHandler("katana", katana)
+MUTINY_SEARCH_HANDLER = DisableAbleCommandHandler("mutiny", mutiny)
 BUTTON_HANDLER = CallbackQueryHandler(button, pattern='anime_.*')
 
 dispatcher.add_handler(BUTTON_HANDLER)
@@ -661,16 +661,16 @@ dispatcher.add_handler(KAIZOKU_SEARCH_HANDLER)
 dispatcher.add_handler(KAYO_SEARCH_HANDLER)
 dispatcher.add_handler(VIBE_SEARCH_HANDLER)
 dispatcher.add_handler(ERAI_SEARCH_HANDLER)
-dispatcher.add_handler(KATANA_HANDLER)
+dispatcher.add_handler(MUTINY_SEARCH_HANDLER)
 dispatcher.add_handler(UPCOMING_HANDLER)
 
 __mod_name__ = "Anime"
 __command_list__ = [
     "anime", "manga", "character", "user", "upcoming", "kaizoku", "airing",
-    "kayo", "vibe", "erai", "katana",
+    "kayo", "vibe", "erai", "mutiny",
 ]
 __handlers__ = [
     ANIME_HANDLER, CHARACTER_HANDLER, MANGA_HANDLER, USER_HANDLER,
     UPCOMING_HANDLER, KAIZOKU_SEARCH_HANDLER, KAYO_SEARCH_HANDLER,
-    BUTTON_HANDLER, AIRING_HANDLER, VIBE_SEARCH_HANDLER, ERAI_SEARCH_HANDLER, KATANA_HANDLER
+    BUTTON_HANDLER, AIRING_HANDLER, VIBE_SEARCH_HANDLER, ERAI_SEARCH_HANDLER, MUTINY_SEARCH_HANDLER
 ]
