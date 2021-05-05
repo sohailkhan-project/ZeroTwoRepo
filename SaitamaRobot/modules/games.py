2d9251cc3e0e16b5c0bad6d1bfcc02f4ba337f66
Dark Samurai <73326139+ninja7-bot@users.noreply.github.com>
2021-05-05 19:48:26 +0530

    Update games.py

--- SaitamaRobot/modules/games.py
+++ SaitamaRobot/modules/games.py
@@ -30,6 +30,14 @@ def tord(update: Update, context: CallbackContext):
 @run_async
 def wyr(update: Update, context: CallbackContext):
     update.effective_message.reply_text(random.choice(fun_strings.WYR_STRINGS))
+    
+TNG = "https://telegra.ph/file/2632bf79c7a184cac562c.png"    
+    
+@run_async
+def tng(update, context):
+    number = random.choice(range(1, 147))
+    update.effective_message.reply_photo(TNG, caption=number,
+                        parse_mode=ParseMode.MARKDOWN)    
 
 
 __help__ = """
@@ -37,23 +45,26 @@ __help__ = """
  • `/dare`*:* gives you a dare
  • `/tord`*:* can be a truth or a dare
  • `/rather`*:* would you rather
+ • `/tng`*:* TNG: The Number Game, answer the question respective to the number.
   """
 
 TRUTH_HANDLER = DisableAbleCommandHandler("truth", truth)
 DARE_HANDLER = DisableAbleCommandHandler("dare", dare)
 TORD_HANDLER = DisableAbleCommandHandler("tord", tord)
 WYR_HANDLER = DisableAbleCommandHandler("rather", wyr)
+TNG_HANDLER = DisableAbleCommandHandler("tng", tng)
 
 dispatcher.add_handler(TRUTH_HANDLER)
 dispatcher.add_handler(DARE_HANDLER)
 dispatcher.add_handler(TORD_HANDLER)
 dispatcher.add_handler(WYR_HANDLER)
+dispatcher.add_handler(TNG_HANDLER)
 
 __mod_name__ = "Games"
 __command_list__ = [
-   "truth", "dare", "tord", "rather",
+   "truth", "dare", "tord", "rather", "tng"
 ]
 
 __handlers__ = [
-    TRUTH_HANDLER, DARE_HANDLER, TORD_HANDLER, WYR_HANDLER,
+    TRUTH_HANDLER, DARE_HANDLER, TORD_HANDLER, WYR_HANDLER, TNG_HANDLER
 ]
