f8aa0b8f5949854126d8eeb0449577f88751a22b
Dark Samurai <73326139+ninja7-bot@users.noreply.github.com>
2021-05-16 17:35:51 +0530

    Update and rename sed.txt to sed.py

--- SaitamaRobot/modules/sed.py
+++ SaitamaRobot/modules/sed.py
@@ -126,8 +126,6 @@ If you want to use these characters, make sure you escape them!
 *Example:* \\?.
 """.format(telegram.MAX_MESSAGE_LENGTH)
 
-__mod_name__ = "Sed/Regex"
-
 SED_HANDLER = DisableAbleMessageHandler(
     Filters.regex(r's([{}]).*?\1.*'.format("".join(DELIMITERS))),
     sed,
